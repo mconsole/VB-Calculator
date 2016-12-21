d080633d9bb18de5aa48c141f5cd893227b52a31
Mitchell Console <mconsole@logs.com>
2016-12-21 14:19:10 -0500

    First commit

---
+++ VB_Calculator/Form1.Designer.vb
@@ -0,0 +1,335 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.decimalBtn = New System.Windows.Forms.Button()
+        Me.clearBtn = New System.Windows.Forms.Button()
+        Me.equalsBtn = New System.Windows.Forms.Button()
+        Me.divideBtn = New System.Windows.Forms.Button()
+        Me.multiplyBtn = New System.Windows.Forms.Button()
+        Me.minusBtn = New System.Windows.Forms.Button()
+        Me.addBtn = New System.Windows.Forms.Button()
+        Me.displayBox = New System.Windows.Forms.RichTextBox()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(41, 156)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(46, 42)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 156)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(46, 42)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(145, 156)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(46, 42)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(41, 204)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(46, 42)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(93, 204)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(46, 42)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(145, 204)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(46, 42)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(41, 252)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(46, 42)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(93, 252)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(46, 42)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(145, 252)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(46, 42)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(41, 300)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(98, 42)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "0"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(287, 24)
+        Me.MenuStrip1.TabIndex = 10
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AboutToolStripMenuItem.Text = "&About..."
+        '
+        'decimalBtn
+        '
+        Me.decimalBtn.Location = New System.Drawing.Point(145, 300)
+        Me.decimalBtn.Name = "decimalBtn"
+        Me.decimalBtn.Size = New System.Drawing.Size(46, 42)
+        Me.decimalBtn.TabIndex = 12
+        Me.decimalBtn.Text = "."
+        Me.decimalBtn.UseVisualStyleBackColor = True
+        '
+        'clearBtn
+        '
+        Me.clearBtn.Location = New System.Drawing.Point(145, 108)
+        Me.clearBtn.Name = "clearBtn"
+        Me.clearBtn.Size = New System.Drawing.Size(46, 42)
+        Me.clearBtn.TabIndex = 13
+        Me.clearBtn.Text = "C"
+        Me.clearBtn.UseVisualStyleBackColor = True
+        '
+        'equalsBtn
+        '
+        Me.equalsBtn.Location = New System.Drawing.Point(197, 300)
+        Me.equalsBtn.Name = "equalsBtn"
+        Me.equalsBtn.Size = New System.Drawing.Size(46, 42)
+        Me.equalsBtn.TabIndex = 14
+        Me.equalsBtn.Text = "="
+        Me.equalsBtn.UseVisualStyleBackColor = True
+        '
+        'divideBtn
+        '
+        Me.divideBtn.Location = New System.Drawing.Point(197, 252)
+        Me.divideBtn.Name = "divideBtn"
+        Me.divideBtn.Size = New System.Drawing.Size(46, 42)
+        Me.divideBtn.TabIndex = 15
+        Me.divideBtn.Text = "/"
+        Me.divideBtn.UseVisualStyleBackColor = True
+        '
+        'multiplyBtn
+        '
+        Me.multiplyBtn.Location = New System.Drawing.Point(197, 204)
+        Me.multiplyBtn.Name = "multiplyBtn"
+        Me.multiplyBtn.Size = New System.Drawing.Size(46, 42)
+        Me.multiplyBtn.TabIndex = 16
+        Me.multiplyBtn.Text = "x"
+        Me.multiplyBtn.UseVisualStyleBackColor = True
+        '
+        'minusBtn
+        '
+        Me.minusBtn.Location = New System.Drawing.Point(197, 156)
+        Me.minusBtn.Name = "minusBtn"
+        Me.minusBtn.Size = New System.Drawing.Size(46, 42)
+        Me.minusBtn.TabIndex = 17
+        Me.minusBtn.Text = "-"
+        Me.minusBtn.UseVisualStyleBackColor = True
+        '
+        'addBtn
+        '
+        Me.addBtn.Location = New System.Drawing.Point(197, 108)
+        Me.addBtn.Name = "addBtn"
+        Me.addBtn.Size = New System.Drawing.Size(46, 42)
+        Me.addBtn.TabIndex = 18
+        Me.addBtn.Text = "+"
+        Me.addBtn.UseVisualStyleBackColor = True
+        '
+        'displayBox
+        '
+        Me.displayBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.displayBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.displayBox.Location = New System.Drawing.Point(41, 41)
+        Me.displayBox.MaxLength = 10
+        Me.displayBox.Multiline = False
+        Me.displayBox.Name = "displayBox"
+        Me.displayBox.Size = New System.Drawing.Size(202, 54)
+        Me.displayBox.TabIndex = 19
+        Me.displayBox.Text = ""
+        Me.displayBox.WordWrap = False
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(93, 108)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(46, 42)
+        Me.Button11.TabIndex = 20
+        Me.Button11.Text = "CE"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(41, 108)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(46, 42)
+        Me.Button12.TabIndex = 21
+        Me.Button12.Text = "←"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(287, 371)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.displayBox)
+        Me.Controls.Add(Me.addBtn)
+        Me.Controls.Add(Me.minusBtn)
+        Me.Controls.Add(Me.multiplyBtn)
+        Me.Controls.Add(Me.divideBtn)
+        Me.Controls.Add(Me.equalsBtn)
+        Me.Controls.Add(Me.clearBtn)
+        Me.Controls.Add(Me.decimalBtn)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents decimalBtn As Button
+    Friend WithEvents clearBtn As Button
+    Friend WithEvents equalsBtn As Button
+    Friend WithEvents divideBtn As Button
+    Friend WithEvents multiplyBtn As Button
+    Friend WithEvents minusBtn As Button
+    Friend WithEvents addBtn As Button
+    Friend WithEvents displayBox As RichTextBox
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+End Class
---
+++ VB_Calculator/Form1.vb
@@ -0,0 +1,144 @@
+﻿Public Class Form1
+    Dim firstNumber As Integer
+    Dim secondNumber As Integer
+    Dim thirdNumber As Integer
+    Dim operatorType
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        displayBox.Text += "1"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles decimalBtn.Click
+        displayBox.Text += "."
+    End Sub
+
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MenuStrip1.ItemClicked
+
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show("Created by Mitchell Console 2016")
+    End Sub
+
+    Private Sub clearBtn_Click(sender As Object, e As EventArgs) Handles clearBtn.Click
+        displayBox.Clear()
+        firstNumber = 0
+        secondNumber = 0
+        thirdNumber = 0
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles displayBox.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        displayBox.Text += "2"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        displayBox.Text += "3"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        displayBox.Text += "4"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        displayBox.Text += "5"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        displayBox.Text += "6"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        displayBox.Text += "7"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        displayBox.Text += "8"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        displayBox.Text += "9"
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        displayBox.Text += "0"
+    End Sub
+
+    Private Sub equalsBtn_Click(sender As Object, e As EventArgs) Handles equalsBtn.Click
+        secondNumber = Integer.Parse(displayBox.Text)
+        If operatorType = "+" Then
+            thirdNumber = firstNumber + secondNumber
+        ElseIf operatorType = "-" Then
+            thirdNumber = firstNumber - secondNumber
+        ElseIf operatorType = "*" Then
+            thirdNumber = firstNumber * secondNumber
+        ElseIf operatorType = "\" Then
+            thirdNumber = firstNumber \ secondNumber
+        End If
+
+        If firstNumber = 0 Then
+            displayBox.Text = secondNumber
+        Else
+            displayBox.Text = thirdNumber
+        End If
+
+    End Sub
+
+    Private Sub addBtn_Click(sender As Object, e As EventArgs) Handles addBtn.Click
+        firstNumber = Integer.Parse(displayBox.Text)
+        operatorType = "+"
+        displayBox.Clear()
+    End Sub
+
+    Private Sub minusBtn_Click(sender As Object, e As EventArgs) Handles minusBtn.Click
+        firstNumber = Integer.Parse(displayBox.Text)
+        operatorType = "-"
+        displayBox.Clear()
+    End Sub
+
+    Private Sub multiplyBtn_Click(sender As Object, e As EventArgs) Handles multiplyBtn.Click
+        firstNumber = Integer.Parse(displayBox.Text)
+        operatorType = "*"
+        displayBox.Clear()
+    End Sub
+
+    Private Sub divideBtn_Click(sender As Object, e As EventArgs) Handles divideBtn.Click
+        firstNumber = Integer.Parse(displayBox.Text)
+        operatorType = "\"
+        displayBox.Clear()
+    End Sub
+
+    Private Sub HelpToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HelpToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub Button11_Click_1(sender As Object, e As EventArgs) Handles Button11.Click
+        displayBox.Clear()
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Dim displayText = displayBox.Text
+        Dim newDisplayText As String = ""
+
+        For index As Integer = 0 To displayText.Length - 2
+            newDisplayText += displayText(index)
+        Next
+
+        displayBox.Text = newDisplayText
+    End Sub
+End Class
